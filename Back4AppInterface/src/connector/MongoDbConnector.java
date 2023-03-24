cc6aed7607240943a2ccc8515c0217b9a8f3ad80
cipollan <cipollan@gmail.com>
2023-03-24 22:06:07 +0100

    Update MongoDbConnector.java

--- Back4AppInterface/src/connector/MongoDbConnector.java
+++ Back4AppInterface/src/connector/MongoDbConnector.java
@@ -1,6 +1,7 @@
 package connector;
 
 
+import org.apache.log4j.Logger;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
@@ -14,23 +15,40 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
-public class MongoDbConnector {
-    public static void doMonGoConnect(String  args) {
-        // Replace the placeholder with your Atlas connection string
-        String uri = "<connection string>";
+import mainPackage.MagicMain;
 
+public class MongoDbConnector {
+	
+	static Logger logger = Logger.getLogger(MongoDbConnector.class.getName());
+	
+    public void doMonGoConnect(String  args) {
+        // Replace the placeholder with your Atlas connection string
+    	logger.debug ( " BEGIN doMonGoConnect ");
+    	
+    	String uri = "mongodb+srv://cipollan:Cippop8@Cippop8@@cluster0.53g76t2.mongodb.net/?retryWrites=true&w=majority";
+
+    	logger.debug ( " BEGIN doMonGoConnect:" + uri );
+    	
         // Construct a ServerApi instance using the ServerApi.builder() method
         ServerApi serverApi = ServerApi.builder()
                 .version(ServerApiVersion.V1)
                 .build();
+        
+        logger.debug ( " BEGIN doMonGoConnect.serverApi:" + serverApi.toString() );
+        
 
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(new ConnectionString(uri))
                 .serverApi(serverApi)
                 .build();
+        
+        logger.debug ( " BEGIN doMonGoConnect.settings:" + settings.toString() );
 
         // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
+        try 
+        {
+        	
+            MongoClient mongoClient = MongoClients.create(settings);
             MongoDatabase database = mongoClient.getDatabase("admin");
             try {
                 // Send a ping to confirm a successful connection
@@ -40,6 +58,13 @@ public class MongoDbConnector {
             } catch (MongoException me) {
                 System.err.println(me);
             }
+            
+            mongoClient.close();
+            
+        }
+        finally 
+        {
+        	System.out.println("finally");
         }
     }
 }
